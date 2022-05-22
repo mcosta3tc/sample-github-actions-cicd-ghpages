0ab829ba0ec691613a8ddc5adb9023fbfbf67581
msagres <m_costa3@etu-webschoolfactory.fr>
2022-05-22 14:27:02 +0200

    chuck-jokes.component.spec.ts fix

--- src/app/chuck-jokes/chuck-jokes.component.spec.ts
+++ src/app/chuck-jokes/chuck-jokes.component.spec.ts
@@ -1,25 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { ChuckJokesComponent } from './chuck-jokes.component';
 
-describe('ChuckJokesComponent', () => {
-  let component: ChuckJokesComponent;
-  let fixture: ComponentFixture<ChuckJokesComponent>;
+describe( 'ChuckJokesComponent', () => {
+    let component: ChuckJokesComponent;
+    let fixture: ComponentFixture<ChuckJokesComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ChuckJokesComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach( async () => {
+        await TestBed.configureTestingModule( {
+            imports: [ HttpClientTestingModule ],
+            declarations: [ ChuckJokesComponent ]
+        } )
+            .compileComponents();
+    } );
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ChuckJokesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach( () => {
+        fixture = TestBed.createComponent( ChuckJokesComponent );
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    } );
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    it( 'should create', () => {
+        expect( component ).toBeTruthy();
+    } );
+} );
